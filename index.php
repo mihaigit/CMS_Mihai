914ec6f46cdca3e5ae3f349a89e1aa1c657155d2
mgit <mihaiw3net@gmail.com>
2014-12-18 00:02:06 +0200

    Started implementation for navigation menu

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
 <body>
 <div><?php include("includes/header.php"); ?></div>
 <div><?php include("includes/navbar.php"); ?></div>
+<div><?php include("includes/navmenu.php"); ?></div>
 <div><?php include("includes/main_content.php"); ?></div>
 <div><?php include("includes/sidebar.php"); ?></div>
+
 
 
 
